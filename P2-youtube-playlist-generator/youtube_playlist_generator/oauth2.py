fafc1fb75fc67f7da280994570a996224fe9785c
hyehae <sdahye908@gmail.com>
2024-05-19 01:55:54 +0900

    Create oauth2.py

---
+++ P2-youtube-playlist-generator/youtube_playlist_generator/oauth2.py
@@ -0,0 +1,32 @@
+import os
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+import googleapiclient.errors
+
+# OAuth 2.0 클라이언트 ID 파일 경로
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# API 서비스 이름 및 버전
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+# 권한 범위
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+
+def get_authenticated_service():
+    try:
+        credentials = Credentials.from_authorized_user_file(CLIENT_SECRETS_FILE)
+    except ValueError as e: # first run with new secret.json (no refresh_token yet)
+        flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+        credentials = flow.run_local_server()
+        with open(CLIENT_SECRETS_FILE, 'w') as file:
+            file.write(credentials.to_json())
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+if __name__ == "__main__":
+    service = get_authenticated_service()
+
